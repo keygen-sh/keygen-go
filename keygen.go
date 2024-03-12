820ab2906013214b48226c355c3b327a20e08af8
Zeke Gabrielse <ezekg@yahoo.com>
2024-03-12 10:21:49 -0500

    bump api version

--- keygen.go
+++ keygen.go
@@ -18,7 +18,7 @@ var (
 	APIURL = "https://api.keygen.sh"
 
 	// APIVersion is the currently supported API version.
-	APIVersion = "1.3"
+	APIVersion = "1.5"
 
 	// APIPrefix is the major version prefix included in all API URLs.
 	APIPrefix = "v1"
